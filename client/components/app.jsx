917a4ae8289da74bf2d1dbce13cedd803bb85304
James Dinh <jdinh8124@gmail.com>
2021-03-04 20:08:38 -0800

    completed initial bootstrap of new project

---
+++ client/components/app.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import { fromFetch } from "rxjs/fetch";
+
+
+export default function App() {
+    const [displayMessage, setDisplayMessage] = useState("Loading...");
+
+    useEffect(() => {
+        getDatasetId();
+      }, []);
+
+    return (
+        <div>
+          <h1>{displayMessage}</h1>
+        </div>
+      );
+    }
